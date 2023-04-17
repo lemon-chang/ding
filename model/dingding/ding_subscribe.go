cf7913af5153fbaa3ad124c10b60cae4e747b935
lihuawei <3476511424@qq.com>
2023-04-17 11:29:36 +0800

    测试群会话人员参数获取

--- model/dingding/ding_subscribe.go
+++ model/dingding/ding_subscribe.go
@@ -53,14 +53,14 @@ func (s *DingSubscribe) UserAddOrg(c *gin.Context) {
 	userIdJson := s.EventJson["UserId"]
 	userIdStrs := userIdJson.([]interface{})
 	user.UserId = userIdStrs[0].(string)
-	fmt.Printf("----user.UserId: %v\n", user.UserId)
+	zap.L().Info("user.UserId: " + user.UserId)
 	token, err2 := user.DingToken.GetAccessToken()
 	if err2 != nil {
 		zap.L().Error("user.DingToken.GetAccessToken() failed ,err: ", zap.Error(err2))
 	} else {
 		zap.L().Info("user.DingToken.GetAccessToken() success ,token: " + token)
 	}
-	user.Token = token
+	user.DingToken.Token = token
 	dingUser, _ := user.GetUserDetailByUserId()
 	//输出用户信息，UserId和Token
 	fmt.Printf("dingUser: %v\n", dingUser)
@@ -79,14 +79,14 @@ func (s *DingSubscribe) UserLeaveOrg(c *gin.Context) {
 	userIdJson := s.EventJson["UserId"]
 	userIdStrs := userIdJson.([]interface{})
 	user.UserId = userIdStrs[0].(string)
-	fmt.Printf("----user.UserId: %v\n", user.UserId)
+	zap.L().Info("user.UserId: " + user.UserId)
 	token, err2 := user.DingToken.GetAccessToken()
 	if err2 != nil {
 		zap.L().Error("user.DingToken.GetAccessToken() failed ,err: ", zap.Error(err2))
 	} else {
 		zap.L().Info("user.DingToken.GetAccessToken() success ,token: " + token)
 	}
-	user.Token = token
+	user.DingToken.Token = token
 	dingUser, _ := user.GetUserDetailByUserId()
 	//输出用户信息，UserId和Token
 	fmt.Printf("dingUser: %v\n", dingUser)
